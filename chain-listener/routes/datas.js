507d26903666b3637b25871834bc01321b185d7c
António Pedro Gonçalves <antoniopedrogoncalves@L15-fanzone1.local>
2021-09-14 17:44:07 +0100

    Remove duplicate route

--- chain-listener/routes/datas.js
+++ chain-listener/routes/datas.js
@@ -23,14 +23,4 @@ router.get("/:key", async (req, res) => {
     }
 })
 
-// Get Data by key
-router.get("/:key", async (req, res) => {
-    try {
-        const data = await Data.findById(req.params.key) || {};
-        res.status(200).json(data); 
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-})
-
 module.exports = router;
